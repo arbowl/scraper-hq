4cb4151fa22d3860daf5186021a1456830e15480
arbowl <arbowl@tutanota.com>
2025-08-30 09:36:53 -0400

    Expand source diversity

--- app.py
+++ app.py
@@ -837,4 +837,4 @@ app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8081, reload=True)
